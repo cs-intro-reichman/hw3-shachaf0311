a617000145b13e40a78925429e5853a12d073e1f
shachaf kuyumgiski <shachaf.kuyumgiski@post.runi.ac.il>
2025-11-12 17:17:18 +0200

    first

--- Anagram.java
+++ Anagram.java
@@ -6,6 +6,7 @@ public class Anagram {
 		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
 		System.out.println(isAnagram("Madam Curie","Radium came")); // true
 		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+		System.out.println(isAnagram("Hello","World")); // false
 
 		// Tests the preProcess function.
 		System.out.println(preProcess("What? No way!!!"));
@@ -28,22 +29,84 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String s1 = preProcess(str1);
+		String s1_extra_proccesed = "";
+		for (int i = 0; i < s1.length(); i++) 		
+		{
+			char ch = s1.charAt(i);
+			if (Character.isLetter(ch)) 
+			{
+				s1_extra_proccesed += Character.toLowerCase(ch);
+			}
+		}
+		String s2 = preProcess(str2);
+		String s2_extra_proccesed = "";
+		for (int i = 0; i < s2.length(); i++) 		
+		{
+			char ch = s2.charAt(i);
+			if (Character.isLetter(ch)) 
+			{
+				s2_extra_proccesed += Character.toLowerCase(ch);
+			}
+		}
+		if (s1_extra_proccesed.length() != s2_extra_proccesed.length())
+		{
+			return false;
+		}
+		for (int i = 0; i < s1_extra_proccesed.length(); i++)
+		{
+			char ch = s1_extra_proccesed.charAt(i);
+			int count1 = 0;
+			int count2 = 0;
+			for (int j = 0; j < s1_extra_proccesed.length(); j++)
+			{
+				if (s1_extra_proccesed.charAt(j) == ch)
+				{
+					count1++;
+				}
+				if (s2_extra_proccesed.charAt(j) == ch)
+				{
+					count2++;
+				}
+			}
+			if (count1 != count2)
+			{
+				return false;
+			}
+		}
+
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String result = "";
+		for (int i = 0; i < str.length(); i++) 		
+		{
+			char ch = str.charAt(i);
+			if (Character.isLetter(ch) || ch == ' ') 
+			{
+				result += Character.toLowerCase(ch);
+			}
+		}
+		return result;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String preprocessedStr = preProcess(str);
+		int len = preprocessedStr.length();
+		if (len == 0) return "";
+		String result = "";
+		for (int i = 0; i < len; i++) 
+		{
+			int randIndex = (int)(Math.random() * preprocessedStr.length());
+			result += preprocessedStr.charAt(randIndex);
+			preprocessedStr = preprocessedStr.substring(0, randIndex) + preprocessedStr.substring(randIndex + 1);
+		}
+		return result;
 	}
 }
